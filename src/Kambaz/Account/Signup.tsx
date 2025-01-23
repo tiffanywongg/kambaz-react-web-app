84077c85b29ae3d5b57e1fba78d1fb58878cd5fa
tiffanywongg <tiffanywong314@gmail.com>
2025-01-23 10:56:54 -0500

    first commit #4

--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -8,6 +8,7 @@ export default function Signup() {
           <input placeholder="verify password" type="password" className="wd-password-verify" /><br/>
           <Link  to="/Kambaz/Account/Profile" > Sign up </Link><br />
           <Link  to="/Kambaz/Account/Signin" >Sign in</Link>
+          
         </div>
     );}
     
