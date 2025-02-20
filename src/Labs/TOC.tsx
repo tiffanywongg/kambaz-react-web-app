eaea46c439de7a55e7030d2cd2557975b8f6b2b0
tiffanywongg <tiffanywong314@gmail.com>
2025-02-19 19:09:04 -0500

    a3 JavaScriptt

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -12,7 +12,7 @@ export default function TOC() {
   <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab3" id="wd-a3"
       active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
   <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3">Kambaz</Nav.Link> </Nav.Item>
-  <Nav.Item> <Nav.Link href="https://github.com/tiffanywongg/kambaz-react-web-app" target="_blank">My GitHub</Nav.Link> </Nav.Item>
+  <Nav.Item> <Nav.Link href="https://github.com/tiffanywongg/kambaz-react-web-app" target="_blank" id="wd-github">My GitHub</Nav.Link> </Nav.Item>
 </Nav>
 );}
   
