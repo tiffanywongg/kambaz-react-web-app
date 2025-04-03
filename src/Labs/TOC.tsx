4395b5768e34d72d66a9c2232744beabb8297cc8
tiffanywongg <tiffanywong314@gmail.com>
2025-04-03 06:05:52 -0400

    a5 api

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -13,10 +13,11 @@ export default function TOC() {
             active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
         <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab4" id="wd-a4"
             active={pathname.includes("Lab4")}> Lab 4 </Nav.Link> </Nav.Item>
-        <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a4"
-            active={pathname.includes("Lab4")}> Lab 5 </Nav.Link> </Nav.Item>
+        <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a5"
+            active={pathname.includes("Lab5")}> Lab 5 </Nav.Link> </Nav.Item>
         <Nav.Item> <Nav.Link as={Link} to="/Kambaz" id="wd-a3">Kambaz</Nav.Link> </Nav.Item>
         <Nav.Item> <Nav.Link href="https://github.com/tiffanywongg/kambaz-react-web-app" target="_blank" id="wd-github">My GitHub</Nav.Link> </Nav.Item>
+        <Nav.Item> <Nav.Link href="https://kambaz-node-server-app.onrender.com" target="_blank" id="wd-node-server">Node Server</Nav.Link> </Nav.Item>
     </Nav>
 );}
   
